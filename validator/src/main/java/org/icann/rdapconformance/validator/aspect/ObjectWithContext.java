1ea43867169ca050299172cdb5ae7a01992cebfd
Julien BERNARD <julien.bernard@viagenie.ca>
2021-03-31 12:09:09 -0400

    Remove aop as it is not relevant anymore

--- validator/src/main/java/org/icann/rdapconformance/validator/aspect/ObjectWithContext.java
+++
@@ -1,8 +0,0 @@
-package org.icann.rdapconformance.validator.aspect;
-
-import org.icann.rdapconformance.validator.RDAPValidatorContext;
-
-public interface ObjectWithContext {
-
-    RDAPValidatorContext getContext();
-}
