6f480c247c9a9e81f866d55f7f2ecefe5c145723
Julien BERNARD <julien.bernard@viagenie.ca>
2021-04-30 09:40:50 -0400

    Return appropriate error when validating entity while thin flag is set

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/rdap/RDAPValidator.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/rdap/RDAPValidator.java
@@ -107,6 +107,11 @@ public abstract class RDAPValidator implements ValidatorWorkflow {
     } else if (RDAPQueryType.NAMESERVERS.equals(queryTypeProcessor.getQueryType())) {
       validator = new SchemaValidator("rdap_nameservers.json", results, datasetService);
     } else if (RDAPQueryType.ENTITY.equals(queryTypeProcessor.getQueryType())) {
+      if (config.isThin()) {
+        logger.error("Thin flag is set while validating entity");
+        query.getStatusCode().ifPresent(rdapValidationResultFile::build);
+        return RDAPValidationStatus.USES_THIN_MODEL.getValue();
+      }
       // asEventActor property is not allow in topMost entity object, see spec 7.2.9.2
       validator = new SchemaValidator("rdap_entity_without_asEventActor.json", results, datasetService);
     }
