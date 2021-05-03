21eaf27fc4412bd1b786622c6344e61874503640
Guillaume Blanchet <guillaume.blanchet@viagenie.ca>
2021-05-03 09:45:37 -0400

    fix: ipAddress schema: v4/v6 properties are arrays not objects

--- validator/src/test/java/org/icann/rdapconformance/validator/schemavalidator/SchemaValidatorIpAddressTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/schemavalidator/SchemaValidatorIpAddressTest.java
@@ -37,19 +37,19 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
   @Test
   public void v4Orv6NotBoth() {
     // add a v6 address to the jsonObject with v4 already:
-    jsonObject.getJSONObject("ipAddress").put("v6", "2001:db8:0000:1:1:1:1:1");
+    jsonObject.getJSONObject("ipAddress").put("v6", List.of("2001:db8:0000:1:1:1:1:1"));
     // validate...
     schemaValidator.validate(jsonObject.toString());
     assertThat(results.getAll())
         .contains(
             RDAPValidationResult.builder()
                 .code(-11401)
-                .value("#/ipAddress/v6:2001:db8:0000:1:1:1:1:1")
+                .value("#/ipAddress/v6:[\"2001:db8:0000:1:1:1:1:1\"]")
                 .message("The name in the name/value pair is not of: v4.")
                 .build(),
             RDAPValidationResult.builder()
                 .code(-11401)
-                .value("#/ipAddress/v4:172.16.254.1")
+                .value("#/ipAddress/v4:[\"172.16.254.1\"]")
                 .message("The name in the name/value pair is not of: v6.")
                 .build()
         );
@@ -60,8 +60,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
    */
   @Test
   public void v4Invalid() {
-    jsonObject.getJSONObject("ipAddress").put("v4", "0.0.0.wrong-ipv4");
-    validate(-11404, "#/ipAddress/v4:0.0.0.wrong-ipv4",
+    jsonObject.getJSONObject("ipAddress").put("v4", List.of("0.0.0.wrong-ipv4"));
+    validate(-11404, "#/ipAddress/v4/0:0.0.0.wrong-ipv4",
         "The v4 structure is not syntactically valid.");
   }
 
@@ -70,8 +70,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
    */
   @Test
   public void v4NotJsonString() {
-    jsonObject.getJSONObject("ipAddress").put("v4", 0);
-    validateIsNotAJsonString(-11405, "#/ipAddress/v4:0");
+    jsonObject.getJSONObject("ipAddress").put("v4", List.of(0));
+    validateIsNotAJsonString(-11405, "#/ipAddress/v4/0:0");
   }
 
   /**
@@ -79,8 +79,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
    */
   @Test
   public void v4NotDotDecimal() {
-    jsonObject.getJSONObject("ipAddress").put("v4", "999");
-    validate(-11406, "#/ipAddress/v4:999",
+    jsonObject.getJSONObject("ipAddress").put("v4", List.of("999"));
+    validate(-11406, "#/ipAddress/v4/0:999",
         "The IPv4 address is not syntactically valid in dot-decimal notation.");
   }
 
@@ -90,8 +90,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
   @Test
   public void v6Invalid() {
     jsonObject.getJSONObject("ipAddress").remove("v4");
-    jsonObject.getJSONObject("ipAddress").put("v6", "0:0:0:wrong-ipv6");
-    validate(-11407, "#/ipAddress/v6:0:0:0:wrong-ipv6",
+    jsonObject.getJSONObject("ipAddress").put("v6", List.of("0:0:0:wrong-ipv6"));
+    validate(-11407, "#/ipAddress/v6/0:0:0:0:wrong-ipv6",
         "The v6 structure is not syntactically valid.");
   }
 
@@ -101,8 +101,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
   @Test
   public void v6NotJsonString() {
     jsonObject.getJSONObject("ipAddress").remove("v4");
-    jsonObject.getJSONObject("ipAddress").put("v6", 0);
-    validateIsNotAJsonString(-11408, "#/ipAddress/v6:0");
+    jsonObject.getJSONObject("ipAddress").put("v6", List.of(0));
+    validateIsNotAJsonString(-11408, "#/ipAddress/v6/0:0");
   }
 
   /**
@@ -111,8 +111,8 @@ public class SchemaValidatorIpAddressTest extends SchemaValidatorTest {
   @Test
   public void v6NotSyntacticallyValid() {
     jsonObject.getJSONObject("ipAddress").remove("v4");
-    jsonObject.getJSONObject("ipAddress").put("v6", "999");
-    validate(-11409, "#/ipAddress/v6:999",
+    jsonObject.getJSONObject("ipAddress").put("v6", List.of("999"));
+    validate(-11409, "#/ipAddress/v6/0:999",
         "The IPv6 address is not syntactically valid.");
   }
 }
