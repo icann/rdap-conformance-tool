035bf618a8e3fcf70b340a50601a50724b4ccbcc
Julien BERNARD <julien.bernard@viagenie.ca>
2021-05-11 16:47:46 -0400

    Add rdapResponseProfile_2_6_3_Validation

---
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/ResponseValidationNoticesIncluded.java
@@ -0,0 +1,45 @@
+package org.icann.rdapconformance.validator.workflow.profile.rdap_response.domain;
+
+import com.jayway.jsonpath.DocumentContext;
+import java.util.List;
+import org.icann.rdapconformance.validator.workflow.profile.ProfileJsonValidation;
+import org.icann.rdapconformance.validator.workflow.rdap.RDAPQueryType;
+import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidationResult;
+import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidatorResults;
+
+public class ResponseValidationNoticesIncluded extends ProfileJsonValidation {
+
+  private final RDAPQueryType queryType;
+
+  public ResponseValidationNoticesIncluded(String rdapResponse,
+      RDAPValidatorResults results,
+      RDAPQueryType queryType) {
+    super(rdapResponse, results);
+    this.queryType = queryType;
+  }
+
+  @Override
+  public String getGroupName() {
+    return "rdapResponseProfile_notices_included_Validation";
+  }
+
+  @Override
+  protected boolean doValidate() {
+    DocumentContext jpath = getJPath();
+    List<?> notices = jpath.read("$..notices");
+    if (notices.isEmpty()) {
+      results.add(RDAPValidationResult.builder()
+          .code(-46500)
+          .value(jsonObject.toString())
+          .message("A notices members does not appear in the RDAP response.")
+          .build());
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public boolean doLaunch() {
+    return queryType.equals(RDAPQueryType.DOMAIN);
+  }
+}
