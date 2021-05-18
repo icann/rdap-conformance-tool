e421769e302e553e856118cd86bca91b362ccc27
Guillaume Blanchet <guillaume.blanchet@viagenie.ca>
2021-05-18 16:16:18 -0400

    fix: validate that valid handle, fn, adr, tel, email members are included and not only that they are included

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/entities/ResponseValidation2Dot7Dot1DotXAndRelated2.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/entities/ResponseValidation2Dot7Dot1DotXAndRelated2.java
@@ -1,7 +1,9 @@
 package org.icann.rdapconformance.validator.workflow.profile.rdap_response.domain.entities;
 
 import java.util.Set;
+import org.everit.json.schema.ValidationException;
 import org.icann.rdapconformance.validator.configuration.RDAPValidatorConfiguration;
+import org.icann.rdapconformance.validator.jcard.JcardCategoriesSchemas;
 import org.icann.rdapconformance.validator.workflow.rdap.RDAPQueryType;
 import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidationResult;
 import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidatorResults;
@@ -38,23 +40,44 @@ public class ResponseValidation2Dot7Dot1DotXAndRelated2 extends
 
   private boolean validateVcardProperty(String jsonPointer, JSONObject entity, String property) {
     Set<String> propertyPointers = getVcardPropertyPointers(entity, property);
+    JcardCategoriesSchemas jcardCategoriesSchemas = new JcardCategoriesSchemas();
     boolean isValid = true;
     if (propertyPointers.isEmpty()) {
       isValid &= log52101(jsonPointer);
-    } else if (property.equals("adr")) {
-      // check if adr contains an address with street (index == 2), City (index == 3) & Country (index == 6)
-      Set<String> adrPointers = getPointerFromJPath(entity,
-          "vcardArray[1][?(@[0]=='adr')][3][2,3,6]");
-      if (adrPointers.size() < 3) {
-        // less than 3 means an address component is missing, we log an error:
-        isValid &= log52101(jsonPointer);
+    } else {
+      for (String propertyPointer : propertyPointers) {
+        try {
+          jcardCategoriesSchemas.getCategory(property).validate(entity.query(propertyPointer));
+        } catch (ValidationException e) {
+          isValid &= log52101(jsonPointer);
+        }
+
+        if (property.equals("adr")) {
+          // check if adr contains an address with street (index == 2), City (index == 3) & Country (index == 6)
+          Set<String> adrPointers = getPointerFromJPath(entity,
+              "vcardArray[1][?(@[0]=='adr')][3][2,3,6]");
+          if (adrPointers.size() < 3) {
+            // less than 3 means an address component is missing, we log an error:
+            isValid &= log52101(jsonPointer);
+          }
+        }
       }
     }
+
+//    else if (property.equals("adr")) {
+//      // check if adr contains an address with street (index == 2), City (index == 3) & Country (index == 6)
+//      Set<String> adrPointers = getPointerFromJPath(entity,
+//          "vcardArray[1][?(@[0]=='adr')][3][2,3,6]");
+//      if (adrPointers.size() < 3) {
+//        // less than 3 means an address component is missing, we log an error:
+//        isValid &= log52101(jsonPointer);
+//      }
+//    }
     return isValid;
   }
 
   private Set<String> getVcardPropertyPointers(JSONObject entity, String property) {
-    return getPointerFromJPath(entity, "vcardArray[1][*][?(@ == '" + property + "')]");
+    return getPointerFromJPath(entity, "vcardArray[1][?(@[0] == '" + property + "')]");
   }
 
   private boolean log52101(String jsonPointer) {
