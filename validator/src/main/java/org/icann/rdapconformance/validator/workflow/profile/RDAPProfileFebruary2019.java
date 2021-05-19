a1648673a2ba5d9456ca6c90e303005fe57a516d
Guillaume Blanchet <guillaume.blanchet@viagenie.ca>
2021-05-18 20:32:07 -0400

    fix: add a try-catch in RDAPProfileFebruary2019 validate method

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/RDAPProfileFebruary2019.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/RDAPProfileFebruary2019.java
@@ -1,9 +1,13 @@
 package org.icann.rdapconformance.validator.workflow.profile;
 
 import java.util.List;
+import org.icann.rdapconformance.validator.SchemaValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RDAPProfileFebruary2019 {
 
+  private static final Logger logger = LoggerFactory.getLogger(RDAPProfileFebruary2019.class);
   private final List<ProfileValidation> validations;
 
   public RDAPProfileFebruary2019(List<ProfileValidation> validations) {
@@ -13,7 +17,13 @@ public class RDAPProfileFebruary2019 {
   public boolean validate() {
     boolean result = true;
     for (ProfileValidation validation : validations) {
-      result &= validation.validate();
+      try {
+        result &= validation.validate();
+      } catch (Exception e) {
+        logger.info("Exception during validation of : {} \n details: {}",
+            validation.getClass().getSimpleName(), e);
+        result = false;
+      }
     }
 
     return result;
--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/entities/ResponseValidation2Dot7Dot1DotXAndRelated2.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/entities/ResponseValidation2Dot7Dot1DotXAndRelated2.java
@@ -63,16 +63,6 @@ public class ResponseValidation2Dot7Dot1DotXAndRelated2 extends
         }
       }
     }
-
-//    else if (property.equals("adr")) {
-//      // check if adr contains an address with street (index == 2), City (index == 3) & Country (index == 6)
-//      Set<String> adrPointers = getPointerFromJPath(entity,
-//          "vcardArray[1][?(@[0]=='adr')][3][2,3,6]");
-//      if (adrPointers.size() < 3) {
-//        // less than 3 means an address component is missing, we log an error:
-//        isValid &= log52101(jsonPointer);
-//      }
-//    }
     return isValid;
   }
 
