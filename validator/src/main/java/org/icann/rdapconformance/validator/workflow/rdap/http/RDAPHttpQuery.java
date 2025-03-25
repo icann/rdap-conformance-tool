b240773eb3e4983753dd40f79fe1c287bd6a5090
bigzj <zhangjin1@gmail.com>
2025-03-25 10:20:46 -0700

    bug/RCT-106-107: system should stop validation after writing the error 13001 and 13002 to the result file and exit with code 0.
    https://icann-jira.atlassian.net/browse/RCT-106
    https://icann-jira.atlassian.net/browse/RCT-107

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/rdap/http/RDAPHttpQuery.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/rdap/http/RDAPHttpQuery.java
@@ -28,6 +28,7 @@ public class RDAPHttpQuery implements RDAPQuery {
   private HttpResponse<String> httpResponse = null;
   private RDAPValidationStatus status = null;
   private JsonData jsonResponse = null;
+  private boolean isQuerySuccessful = true;
 
 
   public RDAPHttpQuery(RDAPValidatorConfiguration config) {
@@ -75,10 +76,6 @@ public class RDAPHttpQuery implements RDAPQuery {
      * not validation on the contents) for a search query, code error -13003 added in results file.
      */
     if (httpResponse.statusCode() == 200) {
-      if(!jsonResponse.isValid()) {
-        logger.error("The response was not valid JSON");
-        return false;
-      }
       if (queryType.isLookupQuery() && !jsonResponseValid()) {
         logger.error("objectClassName was not found in the topmost object");
         results.add(RDAPValidationResult.builder()
@@ -122,7 +119,7 @@ public class RDAPHttpQuery implements RDAPQuery {
    * Check if we got errors with the RDAP HTTP request.
    */
   private boolean isQuerySuccessful() {
-    return status == null;
+    return status == null && isQuerySuccessful;
   }
 
 
@@ -205,6 +202,9 @@ public class RDAPHttpQuery implements RDAPQuery {
                       .value("response body not given")
                       .message("The response was not valid JSON.")
                       .build());
+
+      isQuerySuccessful = false;
+      return;
     }
 
     /* If a response is available to the tool, but the HTTP status code is not 200 nor 404, error
@@ -217,6 +217,8 @@ public class RDAPHttpQuery implements RDAPQuery {
           .value(String.valueOf(httpStatusCode))
           .message("The HTTP status code was neither 200 nor 404.")
           .build());
+
+      isQuerySuccessful = false;
     }
   }
 
