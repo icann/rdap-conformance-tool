6ee496d94ad5271b9f4df872ca0f4078a5e47a44
bigzj <zhangjin1@gmail.com>
2025-03-31 09:59:48 -0700

    feat/RCT-48: 62000 and 62001 validation
    https://icann-jira.atlassian.net/browse/RCT-48

---
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/general/ResponseValidation1Dot2_2_2024Test.java
@@ -0,0 +1,23 @@
+package org.icann.rdapconformance.validator.workflow.profile.rdap_response.general;
+
+import java.util.List;
+import org.icann.rdapconformance.validator.workflow.profile.RDAPConformanceValidation;
+import org.icann.rdapconformance.validator.workflow.profile.RDAPConformanceValidationTest;
+
+public class ResponseValidation1Dot2_2_2024Test extends RDAPConformanceValidationTest {
+
+    public ResponseValidation1Dot2_2_2024Test() {
+        super("rdapResponseProfile_1_2_Validation");
+    }
+
+    @Override
+    public void testValidate_ok() {
+        jsonObject.put("rdapConformance", List.of("rdap_level_0", "icann_rdap_technical_implementation_guide_1", "icann_rdap_response_profile_1", "redacted"));
+        validate();
+    }
+
+    @Override
+    public RDAPConformanceValidation getProfileValidation() {
+        return new ResponseValidation1Dot2_2_2024(jsonObject.toString(), results);
+    }
+}
