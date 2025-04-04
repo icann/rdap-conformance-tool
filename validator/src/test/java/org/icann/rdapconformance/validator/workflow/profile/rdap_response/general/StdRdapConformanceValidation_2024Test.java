5201af09a38706cacd80c9854ed72f5b16e993d2
bigzj <zhangjin1@gmail.com>
2025-04-04 09:57:50 -0700

    bug/RCT-38: 10612 and 10613 validation
    https://icann-jira.atlassian.net/browse/RCT-38

---
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/general/StdRdapConformanceValidation_2024Test.java
@@ -0,0 +1,33 @@
+package org.icann.rdapconformance.validator.workflow.profile.rdap_response.general;
+
+import java.util.List;
+import org.icann.rdapconformance.validator.workflow.profile.ProfileJsonValidation;
+import org.icann.rdapconformance.validator.workflow.profile.ProfileJsonValidationTestBase;
+import org.testng.annotations.Test;
+
+public class StdRdapConformanceValidation_2024Test  extends ProfileJsonValidationTestBase {
+
+
+    public StdRdapConformanceValidation_2024Test() {
+        super("/validators/domain/valid.json", "stdRdapConformanceValidation");
+    }
+
+    @Override
+    public ProfileJsonValidation getProfileValidation() {
+        return new StdRdapConformanceValidation_2024(jsonObject.toString(), results);
+    }
+
+    @Test
+    public void testValidate_ContainsRdapConformance_AddResults10612() {
+        removeKey("rdapConformance");
+        validate(-10612, jsonObject.toString(),
+            "RFC 9083 requires all RDAP responses to have an rdapConformance array.");
+    }
+
+    @Test
+    public void testValidate_ContainsRdapConformanceInChild_AddResults10613() {
+        putValue("$.secureDNS", "rdapConformance", List.of("dummy"));
+        validate(-10613, "#/secureDNS/rdapConformance",
+            "The rdapConformance array must appear only in the top-most of the RDAP response.");
+    }
+}
