416e457bea94bce07c4e8b635b11fcc9fc27a732
Adam Guyot <adam@cobenian.com>
2025-04-16 15:16:57 -0400

    updated the test

--- validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/RDAPValidationResultFileTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/RDAPValidationResultFileTest.java
@@ -1,5 +1,6 @@
 package org.icann.rdapconformance.validator.workflow.rdap;
 
+import static java.net.HttpURLConnection.HTTP_OK;
 import static org.icann.rdapconformance.validator.exception.parser.ExceptionParser.UNKNOWN_ERROR_CODE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.contains;
@@ -19,80 +20,97 @@ import org.testng.annotations.Test;
 
 public class RDAPValidationResultFileTest {
 
-//  private FileSystem fileSystem;
-//  private RDAPValidationResultFile file;
-//  private RDAPValidatorResults results;
-//  private ConfigurationFile configurationFile;
-//
-//  @BeforeMethod
-//  public void setUp() {
-//    results = RDAPValidatorResultsImpl.getInstance();
-//    results.clear();
-//    fileSystem = mock(FileSystem.class);
-//    results.addGroups(Set.of("firstGroup"));
-//    configurationFile = mock(ConfigurationFile.class);
-//    file = new RDAPValidationResultFile(
-//        results,
-//        mock(RDAPValidatorConfiguration.class),
-//        configurationFile,
-//        fileSystem);
-//  }
-//
-//  @Test
-//  public void testGroupOkAssigned() throws IOException {
-//    file.build(200);
-//    verify(fileSystem).write(any(), contains("\"groupOK\": [\"firstGroup\"]"));
-//  }
-//
-//  @Test
-//  public void testGroupErrorWarningAssigned() throws IOException {
-//    results.addGroupErrorWarning("secondGroup");
-//    file.build(200);
-//    verify(fileSystem).write(any(), contains("\"groupErrorWarning\": [\"secondGroup\"]"));
-//  }
-//
-//  @Test
-//  public void testIgnore() throws IOException {
-//    int ignoredCode = -1000;
-//    results.add(RDAPValidationResult.builder()
-//        .code(ignoredCode)
-//        .value("ignoreCode")
-//        .message("this is a code to ignore")
-//        .build());
-//    results.add(RDAPValidationResult.builder()
-//        .code(UNKNOWN_ERROR_CODE)
-//        .value("unknown_code")
-//        .message("We log unknown error code, but they aren't part of the result file")
-//        .build());
-//    doReturn(List.of(ignoredCode)).when(configurationFile).getDefinitionIgnore();
-//    file.build(200);
-//    // error should be an empty list since the only result code must be ignored:
-//    verify(fileSystem).write(any(), contains("\"error\": []"));
-//  }
-//
-//  @Test
-//  public void testResultsFilePath() throws IOException {
-//    RDAPValidatorConfiguration config = mock(RDAPValidatorConfiguration.class);
-//    String customResultsFilePath = "custom_results.json";
-//    doReturn(customResultsFilePath).when(config).getResultsFile();
-//
-//    file = new RDAPValidationResultFile(results, config, configurationFile, fileSystem);
-//    file.build(200);
-//
-//    // Verify that the results are written to the custom file path
-//    verify(fileSystem).write(eq(customResultsFilePath), any(String.class));
-//  }
-//
-//  @Test
-//  public void testDefaultResultsFilePath() throws IOException {
-//    RDAPValidatorConfiguration config = mock(RDAPValidatorConfiguration.class);
-//    doReturn(null).when(config).getResultsFile();
-//
-//    file = new RDAPValidationResultFile(results, config, configurationFile, fileSystem);
-//    file.build(200);
-//
-//    // Verify that the results are written to the default file path
-//    verify(fileSystem).mkdir("results");
-//    verify(fileSystem).write(contains("results/results-"), any(String.class));
-//  }
+    private FileSystem fileSystem;
+    private RDAPValidatorResults results;
+    private ConfigurationFile configurationFile;
+
+    @BeforeMethod
+    public void setUp() {
+        // Reset the singleton instance before each test
+        RDAPValidationResultFile.reset();
+
+        results = RDAPValidatorResultsImpl.getInstance();
+        results.clear();
+        fileSystem = mock(FileSystem.class);
+        results.addGroups(Set.of("firstGroup"));
+        configurationFile = mock(ConfigurationFile.class);
+
+        // Initialize the singleton instance
+        RDAPValidationResultFile.getInstance().initialize(
+            results,
+            mock(RDAPValidatorConfiguration.class),
+            configurationFile,
+            fileSystem
+        );
+    }
+
+    @Test
+    public void testGroupOkAssigned() throws IOException {
+        RDAPValidationResultFile.getInstance().build(HTTP_OK);
+        verify(fileSystem).write(any(), contains("\"groupOK\": [\"firstGroup\"]"));
+    }
+
+    @Test
+    public void testGroupErrorWarningAssigned() throws IOException {
+        results.addGroupErrorWarning("secondGroup");
+        RDAPValidationResultFile.getInstance().build(HTTP_OK);
+        verify(fileSystem).write(any(), contains("\"groupErrorWarning\": [\"secondGroup\"]"));
+    }
+
+    @Test
+    public void testIgnore() throws IOException {
+        int ignoredCode = -1000;
+        results.add(RDAPValidationResult.builder()
+                                        .code(ignoredCode)
+                                        .value("ignoreCode")
+                                        .message("this is a code to ignore")
+                                        .build());
+        results.add(RDAPValidationResult.builder()
+                                        .code(UNKNOWN_ERROR_CODE)
+                                        .value("unknown_code")
+                                        .message("We log unknown error code, but they aren't part of the result file")
+                                        .build());
+        doReturn(List.of(ignoredCode)).when(configurationFile).getDefinitionIgnore();
+        RDAPValidationResultFile.getInstance().build(HTTP_OK);
+        // error should be an empty list since the only result code must be ignored:
+        verify(fileSystem).write(any(), contains("\"error\": []"));
+    }
+
+    @Test
+    public void testResultsFilePath() throws IOException {
+        RDAPValidatorConfiguration config = mock(RDAPValidatorConfiguration.class);
+        String customResultsFilePath = "custom_results.json";
+        doReturn(customResultsFilePath).when(config).getResultsFile();
+
+        RDAPValidationResultFile.reset();
+        RDAPValidationResultFile.getInstance().initialize(
+            results,
+            config,
+            configurationFile,
+            fileSystem
+        );
+        RDAPValidationResultFile.getInstance().build(HTTP_OK);
+
+        // Verify that the results are written to the custom file path
+        verify(fileSystem).write(eq(customResultsFilePath), any(String.class));
+    }
+
+    @Test
+    public void testDefaultResultsFilePath() throws IOException {
+        RDAPValidatorConfiguration config = mock(RDAPValidatorConfiguration.class);
+        doReturn(null).when(config).getResultsFile();
+
+        RDAPValidationResultFile.reset();
+        RDAPValidationResultFile.getInstance().initialize(
+            results,
+            config,
+            configurationFile,
+            fileSystem
+        );
+        RDAPValidationResultFile.getInstance().build(HTTP_OK);
+
+        // Verify that the results are written to the default file path
+        verify(fileSystem).mkdir("results");
+        verify(fileSystem).write(contains("results/results-"), any(String.class));
+    }
 }
