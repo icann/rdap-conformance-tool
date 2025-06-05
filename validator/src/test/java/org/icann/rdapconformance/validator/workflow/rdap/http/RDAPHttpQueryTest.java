e4b2d91b32dfae7fdafad5095feb89b784b2eaeb
Adam Guyot <adam@cobenian.com>
2025-06-05 17:24:00 -0400

    feat/RCT-167-HonorRetryHeader re-enable tests

--- validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/http/RDAPHttpQueryTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/http/RDAPHttpQueryTest.java
@@ -577,26 +577,26 @@ public class RDAPHttpQueryTest extends HttpTestingUtils {
                     .build());
   }
 
-  //  @Test
-  //  public void test_WithJsonArray() {
-  //    String path = "/nameservers?ip=.*";
-  //    String response = "{\"nameserverSearchResults\": [ {\"objectClassName\":\"nameserver\"} ]}";
-  //    RDAPValidatorResults results = RDAPValidatorResultsImpl.getInstance();
-  //    results.clear();
-  //    rdapHttpQuery.setResults(results);
-  //
-  //    givenUri(HTTP, path);
-  //    stubFor(get(urlEqualTo(NAMESERVERS))
-  //        .withScheme(HTTP)
-  //        .willReturn(aResponse()
-  //            .withHeader("Content-Type", "application/rdap+JSON;encoding=UTF-8")
-  //            .withBody(response)));
-  //
-  //    assertThat(rdapHttpQuery.run()).isTrue();
-  //    assertThat(rdapHttpQuery.getData()).isEqualTo(response);
-  //    assertThat(rdapHttpQuery.getStatusCode()).isPresent().get().isEqualTo(200);
-  //    assertThat(rdapHttpQuery.hasNameserverSearchResults()).isTrue();
-  //  }
+    @Test
+    public void test_WithJsonArray() {
+      String path = "/nameservers?ip=.*";
+      String response = "{\"nameserverSearchResults\": [ {\"objectClassName\":\"nameserver\"} ]}";
+      RDAPValidatorResults results = RDAPValidatorResultsImpl.getInstance();
+      results.clear();
+      rdapHttpQuery.setResults(results);
+
+      givenUri(HTTP, path);
+      stubFor(get(urlEqualTo(path))
+          .withScheme(HTTP)
+          .willReturn(aResponse()
+              .withHeader("Content-Type", "application/rdap+JSON;encoding=UTF-8")
+              .withBody(response)));
+
+      assertThat(rdapHttpQuery.run()).isTrue();
+      assertThat(rdapHttpQuery.getData()).isEqualTo(response);
+      assertThat(rdapHttpQuery.getStatusCode()).isPresent().get().isEqualTo(200);
+      assertThat(rdapHttpQuery.hasNameserverSearchResults()).isTrue();
+    }
 
   @Test
   public void checkWithQueryType_JsonResponseIsAnArray_IsOk() {
@@ -642,36 +642,36 @@ public class RDAPHttpQueryTest extends HttpTestingUtils {
                     .build());
   }
 
-//  @Test
-//  public void checkWithQueryType_JsonResponseIsNotAnArray_ReturnsErrorCode12610InResults() {
-//    String path = "/nameservers?ip=.*";
-//    String response = "{\"nameserverSearchResults\": { \"objectClassName\":\"nameserver\" }}";
-//    RDAPValidatorResults results = RDAPValidatorResultsImpl.getInstance();
-//    results.clear();
-//    rdapHttpQuery.setResults(results);
-//
-//    // enable 2024 profile
-//    doReturn(true).when(config).useRdapProfileFeb2024();
-//
-//    givenUri(HTTP, path);
-//    stubFor(get(urlEqualTo(NAMESERVERS))
-//        .withScheme(HTTP)
-//        .willReturn(aResponse()
-//            .withHeader("Content-Type", "application/rdap+JSON;encoding=UTF-8")
-//            .withBody(response)));
-//
-//    assertThat(rdapHttpQuery.run()).isTrue();
-//    assertThat(rdapHttpQuery.checkWithQueryType(RDAPQueryType.NAMESERVERS)).isTrue();
-//    assertThat(results.getAll()).contains(
-//        RDAPValidationResult.builder()
-//            .code(-12610)
-//            .value(response)
-//            .message("The nameserverSearchResults structure is required.")
-//            .build());
-//
-//    // disable 2024 profile after testing
-//    doReturn(false).when(config).useRdapProfileFeb2024();
-//  }
+  @Test
+  public void checkWithQueryType_JsonResponseIsNotAnArray_ReturnsErrorCode12610InResults() {
+    String path = "/nameservers?ip=.*";
+    String response = "{\"nameserverSearchResults\": { \"objectClassName\":\"nameserver\" }}";
+    RDAPValidatorResults results = RDAPValidatorResultsImpl.getInstance();
+    results.clear();
+    rdapHttpQuery.setResults(results);
+
+    // enable 2024 profile
+    doReturn(true).when(config).useRdapProfileFeb2024();
+
+    givenUri(HTTP, path);
+    stubFor(get(urlEqualTo(path))
+        .withScheme(HTTP)
+        .willReturn(aResponse()
+            .withHeader("Content-Type", "application/rdap+JSON;encoding=UTF-8")
+            .withBody(response)));
+
+    assertThat(rdapHttpQuery.run()).isTrue();
+    assertThat(rdapHttpQuery.checkWithQueryType(RDAPQueryType.NAMESERVERS)).isTrue();
+    assertThat(results.getAll()).contains(
+        RDAPValidationResult.builder()
+            .code(-12610)
+            .value(response)
+            .message("The nameserverSearchResults structure is required.")
+            .build());
+
+    // disable 2024 profile after testing
+    doReturn(false).when(config).useRdapProfileFeb2024();
+  }
 
   @Test
   public void jsonResponseValid_ReturnsFalseUsingNoObjectClassName() {
