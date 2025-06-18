d05bf5ca6fbbcdf660a892a37cb2b816f211b964
Adam Guyot <adam@cobenian.com>
2025-06-18 10:43:12 -0400

    feat/RCT-200-EnsureProperExitCodes swap the numbers out

--- validator/src/main/java/org/icann/rdapconformance/validator/ConnectionStatus.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/ConnectionStatus.java
@@ -5,6 +5,7 @@ package org.icann.rdapconformance.validator;
  * Represents the status of a network connection or connection attempt.
  */
 public enum ConnectionStatus implements ConformanceError {
+    // NOTE: these no longer have anything to do with the application exiting.
     SUCCESS(0, "Connection succeeded"),
     CONNECTION_FAILED(10, "Failed to connect to host"),
     HANDSHAKE_FAILED(11, "The TLS handshake failed"),
@@ -17,10 +18,10 @@ public enum ConnectionStatus implements ConformanceError {
     HTTP2_ERROR(18, "HTTP/2 errors"),
     NETWORK_SEND_FAIL(19, "Failure sending network data"),
     NETWORK_RECEIVE_FAIL(20, "Failure in receiving network data"),
-    // 21 is over in ToolResult as FILE_WRITE_ERROR
-    TOO_MANY_REQUESTS(22, "Too many requests"),
+    // 21 is over in ToolResult as FILE_WRITE_ERROR, legacy exit numbers
     UNKNOWN_HOST(23, "Unknown host"),
-    CONNECTION_REFUSED(24, "Connection refused by host");
+    CONNECTION_REFUSED(24, "Connection refused by host"),
+    TOO_MANY_REQUESTS(26, "Too many requests");
 
     private final int code;
     private final String description;
