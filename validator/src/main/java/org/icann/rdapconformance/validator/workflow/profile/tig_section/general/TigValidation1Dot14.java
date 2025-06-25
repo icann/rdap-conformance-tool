ff2bdabdd19d33d18f415d39e94693bc26eec3f3
Adam Guyot <adam@cobenian.com>
2025-06-25 11:29:58 -0400

    bug/RCT-231-ConformanceProfileLogicIsIncorrect fixed the logic to allow for higher number digits

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/tig_section/general/TigValidation1Dot14.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/tig_section/general/TigValidation1Dot14.java
@@ -1,7 +1,10 @@
 package org.icann.rdapconformance.validator.workflow.profile.tig_section.general;
 
 import org.icann.rdapconformance.validator.workflow.profile.RDAPConformanceValidation;
+import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidationResult;
 import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidatorResults;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class TigValidation1Dot14 extends RDAPConformanceValidation {
 
@@ -10,6 +13,44 @@ public class TigValidation1Dot14 extends RDAPConformanceValidation {
     super(rdapResponse, results, "icann_rdap_technical_implementation_guide_0", -20600,
         "The RDAP Conformance data structure does not include icann_rdap_technical_implementation_guide_0. "
             + "See section 1.14 of the RDAP_Technical_Implementation_Guide_2_1.");
+  }
+
+
+  @Override
+  protected boolean doValidate() {
+    String jsonPointer = "#/rdapConformance";
+    JSONArray rdapConformance = (JSONArray) new JSONObject(jsonObject.toString())
+        .query(jsonPointer);
+
+    if (rdapConformance == null) {
+      results.add(RDAPValidationResult.builder()
+                                      .code(-20600)
+                                      .value(getResultValue(jsonPointer))
+                                      .message("The RDAP Conformance data structure does not include icann_rdap_technical_implementation_guide_0. "
+                                          + "See section 1.14 of the RDAP_Technical_Implementation_Guide_2_1.")
+                                      .build());
+      return false;
+    }
+
+    boolean found = false;
+    for (Object v : rdapConformance) {
+      if (v instanceof String && ((String) v).matches("icann_rdap_technical_implementation_guide_\\d+")) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      results.add(RDAPValidationResult.builder()
+                                      .code(-20600)
+                                      .value(getResultValue(jsonPointer))
+                                      .message("The RDAP Conformance data structure does not include icann_rdap_technical_implementation_guide_0. "
+                                          + "See section 1.14 of the RDAP_Technical_Implementation_Guide_2_1.")
+                                      .build());
+      return false;
+    }
+
+    return true;
   }
 
   @Override
