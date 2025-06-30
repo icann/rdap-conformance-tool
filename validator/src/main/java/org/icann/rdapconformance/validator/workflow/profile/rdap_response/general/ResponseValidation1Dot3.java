9416dcd3e55b9ccbfccd739b89a7bc15c0026eb9
Adam Guyot <adam@cobenian.com>
2025-06-30 14:32:37 -0400

    bug/RCT-236-ConformanceProfileRevert revert

--- validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/general/ResponseValidation1Dot3.java
+++ validator/src/main/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/general/ResponseValidation1Dot3.java
@@ -2,9 +2,6 @@ package org.icann.rdapconformance.validator.workflow.profile.rdap_response.gener
 
 import org.icann.rdapconformance.validator.workflow.profile.RDAPConformanceValidation;
 import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidatorResults;
-import org.icann.rdapconformance.validator.workflow.rdap.RDAPValidationResult;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public final class ResponseValidation1Dot3 extends RDAPConformanceValidation {
 
@@ -14,43 +11,6 @@ public final class ResponseValidation1Dot3 extends RDAPConformanceValidation {
             + "See section 1.3 of the RDAP_Response_Profile_2_1.");
   }
 
-  // Ensure that the digit at the end of the profile name can be higher than 0
-@Override
-protected boolean doValidate() {
-    String jsonPointer = "#/rdapConformance";
-    JSONArray rdapConformance = (JSONArray) new JSONObject(jsonObject.toString())
-        .query(jsonPointer);
-
-    if (rdapConformance == null) {
-        results.add(RDAPValidationResult.builder()
-            .code(-40200)
-            .value(getResultValue(jsonPointer))
-            .message("The RDAP Conformance data structure does not include icann_rdap_response_profile_0. "
-                + "See section 1.3 of the RDAP_Response_Profile_2_1.")
-            .build());
-        return false;
-    }
-
-    boolean found = false;
-    for (Object v : rdapConformance) {
-        if (v instanceof String && ((String) v).matches("icann_rdap_response_profile_\\d+")) {
-            found = true;
-            break;
-        }
-    }
-
-    if (!found) {
-        results.add(RDAPValidationResult.builder()
-            .code(-40200)
-            .value(getResultValue(jsonPointer))
-            .message("The RDAP Conformance data structure does not include icann_rdap_response_profile_0. "
-                + "See section 1.3 of the RDAP_Response_Profile_2_1.")
-            .build());
-        return false;
-    }
-
-    return true;
-}
   @Override
   public String getGroupName() {
     return "rdapResponseProfile_1_3_Validation";
