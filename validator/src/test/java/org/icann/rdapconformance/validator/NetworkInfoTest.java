2e6786b11fcc3cf273efb576638732d024f4c2d8
Adam Guyot <adam@cobenian.com>
2025-08-07 14:54:05 -0400

    chore/RCT-296-CodeCoveragePlus singleton pollution

--- validator/src/test/java/org/icann/rdapconformance/validator/NetworkInfoTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/NetworkInfoTest.java
@@ -1,12 +1,14 @@
 package org.icann.rdapconformance.validator;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NetworkInfoTest {
 
+    @BeforeMethod
     @AfterMethod
     public void resetNetworkInfo() {
         NetworkInfo.setAcceptHeaderToApplicationJson();
