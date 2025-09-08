4e72ab56ea0b9e53b7da5a280cfef5972268a081
Adam Guyot <adam@cobenian.com>
2025-09-08 15:07:34 -0400

    RCT-396 - fix: add the missing logic and bump up the test coverage

--- validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/dataset/model/Ipv6AddressSpaceTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/dataset/model/Ipv6AddressSpaceTest.java
@@ -18,6 +18,10 @@ public class Ipv6AddressSpaceTest extends BaseUnmarshallingTest<Ipv6AddressSpace
         this.ipv6AddressSpace = unmarshal("/dataset/ipv6-address-space.xml", Ipv6AddressSpace.class);
     }
 
+    public Ipv6AddressSpace getIpv6AddressSpace() {
+        return ipv6AddressSpace;
+    }
+
     @Test
     public void givenValidIpv6AddressSpaceXml_whenUnmarshalling_thenReturnIpv6AddressSpace() throws
                                                                                              NoSuchFieldException,
@@ -52,6 +56,105 @@ public class Ipv6AddressSpaceTest extends BaseUnmarshallingTest<Ipv6AddressSpace
                 tuple("fe80::/10", "Link-Scoped Unicast"),
                 tuple("fec0::/10", "Reserved by IETF"),
                 tuple("ff00::/8", "Multicast"));
+    }
+
+    @Test
+    public void testIsInvalid_globalUnicastAddresses() {
+        // Test addresses that SHOULD be valid (return false) because they're in Global Unicast (2000::/3)
+        
+        assertThat(ipv6AddressSpace.isInvalid("2001:db8::1"))
+            .as("2001:db8::1 should be valid (in Global Unicast 2000::/3)")
+            .isFalse();
+        
+        assertThat(ipv6AddressSpace.isInvalid("2001:500:7967::30"))
+            .as("2001:500:7967::30 should be valid (in Global Unicast 2000::/3)")
+            .isFalse();
+        
+        assertThat(ipv6AddressSpace.isInvalid("2400:cb00::1"))
+            .as("2400:cb00::1 should be valid (in Global Unicast 2000::/3)")
+            .isFalse();
+        
+        assertThat(ipv6AddressSpace.isInvalid("2800::1"))
+            .as("2800::1 should be valid (in Global Unicast 2000::/3)")
+            .isFalse();
+        
+        assertThat(ipv6AddressSpace.isInvalid("3fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("3fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be valid (end of 2000::/3)")
+            .isFalse();
+    }
+    
+    @Test 
+    public void testIsInvalid_nonGlobalUnicastAddresses() {
+        // Test addresses that SHOULD be invalid (return true) because they're NOT in Global Unicast
+        
+        // Unique Local Unicast (fc00::/7)
+        assertThat(ipv6AddressSpace.isInvalid("fc00::1"))
+            .as("fc00::1 should be invalid (Unique Local Unicast, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (Unique Local)")
+            .isTrue();
+        
+        // Link-Scoped Unicast (fe80::/10)
+        assertThat(ipv6AddressSpace.isInvalid("fe80::1"))
+            .as("fe80::1 should be invalid (Link-Scoped Unicast, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (Link-Scoped)")
+            .isTrue();
+        
+        // Multicast (ff00::/8)
+        assertThat(ipv6AddressSpace.isInvalid("ff00::1"))
+            .as("ff00::1 should be invalid (Multicast, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (Multicast)")
+            .isTrue();
+        
+        // Reserved by IETF ranges
+        assertThat(ipv6AddressSpace.isInvalid("::1"))
+            .as("::1 should be invalid (Reserved by IETF 0000::/8, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("0100::1"))
+            .as("0100::1 should be invalid (Reserved by IETF 0100::/8, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("4000::1"))
+            .as("4000::1 should be invalid (Reserved by IETF 4000::/3, not Global Unicast)")
+            .isTrue();
+        
+        assertThat(ipv6AddressSpace.isInvalid("1fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("1fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (Reserved by IETF 1000::/4)")
+            .isTrue();
+    }
+    
+    @Test
+    public void testIsInvalid_boundaryAddresses() {
+        // Test addresses at the boundaries of Global Unicast range (2000::/3)
+        
+        // Just before Global Unicast
+        assertThat(ipv6AddressSpace.isInvalid("1fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("1fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (just before 2000::/3)")
+            .isTrue();
+        
+        // Start of Global Unicast range - use a well-formed address
+        assertThat(ipv6AddressSpace.isInvalid("2000:0:0:0:0:0:0:1"))
+            .as("2000:0:0:0:0:0:0:1 should be valid (in Global Unicast 2000::/3)")
+            .isFalse();
+        
+        // End of Global Unicast
+        assertThat(ipv6AddressSpace.isInvalid("3fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("3fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be valid (end of 2000::/3)")
+            .isFalse();
+        
+        // Just after Global Unicast
+        assertThat(ipv6AddressSpace.isInvalid("4000::"))
+            .as("4000:: should be invalid (just after 2000::/3, starts 4000::/3)")
+            .isTrue();
     }
 
 }
--- validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/dataset/model/SpecialIPv6AddressesTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/rdap/dataset/model/SpecialIPv6AddressesTest.java
@@ -38,4 +38,78 @@ public class SpecialIPv6AddressesTest extends BaseUnmarshallingTest<SpecialIPv6A
                 "2001:1::1/128",
                 "2001:20::/28");
     }
+
+    @Test
+    public void testIsInvalid_specialPurposeAddresses() {
+        // Test addresses that SHOULD be invalid (return true) because they're in special-purpose registry
+        
+        // Documentation addresses (2001:db8::/32)
+        assertThat(ipAddressSpecialRegistry.isInvalid("2001:db8::1"))
+            .as("2001:db8::1 should be invalid (documentation address)")
+            .isTrue();
+        assertThat(ipAddressSpecialRegistry.isInvalid("2001:db8:1234:5678::abcd"))
+            .as("2001:db8:1234:5678::abcd should be invalid (documentation address)")
+            .isTrue();
+        
+        // Loopback (::1/128)
+        assertThat(ipAddressSpecialRegistry.isInvalid("::1"))
+            .as("::1 should be invalid (loopback)")
+            .isTrue();
+        
+        // Link-Local (fe80::/10)
+        assertThat(ipAddressSpecialRegistry.isInvalid("fe80::1"))
+            .as("fe80::1 should be invalid (link-local)")
+            .isTrue();
+        assertThat(ipAddressSpecialRegistry.isInvalid("febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("febf:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (link-local)")
+            .isTrue();
+        
+        // IETF Protocol Assignments (2001::/32 but more specific than 2001::/23)
+        assertThat(ipAddressSpecialRegistry.isInvalid("2001::1"))
+            .as("2001::1 should be invalid (IETF protocol assignments)")
+            .isTrue();
+        
+        // Unique Local (fc00::/7) - also in special registry
+        assertThat(ipAddressSpecialRegistry.isInvalid("fc00::1"))
+            .as("fc00::1 should be invalid (unique local)")
+            .isTrue();
+        assertThat(ipAddressSpecialRegistry.isInvalid("fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"))
+            .as("fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff should be invalid (unique local)")
+            .isTrue();
+    }
+    
+    @Test
+    public void testIsInvalid_regularGlobalUnicast() {
+        // Test addresses that should be valid (return false) because they're NOT in special-purpose registry
+        // These are regular Global Unicast addresses
+        
+        assertThat(ipAddressSpecialRegistry.isInvalid("2400:cb00::1"))
+            .as("2400:cb00::1 should be valid (regular global unicast)")
+            .isFalse();
+        
+        assertThat(ipAddressSpecialRegistry.isInvalid("2800::1"))
+            .as("2800::1 should be valid (regular global unicast)")
+            .isFalse();
+        
+        assertThat(ipAddressSpecialRegistry.isInvalid("3000::1"))
+            .as("3000::1 should be valid (regular global unicast)")
+            .isFalse();
+        
+        // Test an address that might be close to special ranges but not in them
+        assertThat(ipAddressSpecialRegistry.isInvalid("2003::1"))
+            .as("2003::1 should be valid (outside 2001::/32 and 2001:db8::/32)")
+            .isFalse();
+    }
+    
+    @Test
+    public void testIsInvalid_realWorldCase() {
+        // Test the specific case mentioned in the bug report
+        String icannAddress = "2001:500:7967::30";
+        
+        // This address is NOT in the special-purpose registry based on the actual data
+        // Even though 2001::/23 is in the registry, 2001:500:: appears to be outside that range
+        assertThat(ipAddressSpecialRegistry.isInvalid(icannAddress))
+            .as("2001:500:7967::30 should be valid (not in special-purpose registry)")
+            .isFalse();
+    }
 }
