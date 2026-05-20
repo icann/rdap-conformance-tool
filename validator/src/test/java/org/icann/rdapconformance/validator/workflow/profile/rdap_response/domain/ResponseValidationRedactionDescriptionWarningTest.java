b4646d559f6f2fae7ac7c3a84db6e8a3d7218e47
Eric Alfonso <>
2026-05-20 12:27:26 -0600

    feat/RCT-65811-Update verification count for RDAPValidationResult in tests

--- validator/src/test/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/ResponseValidationRedactionDescriptionWarningTest.java
+++ validator/src/test/java/org/icann/rdapconformance/validator/workflow/profile/rdap_response/domain/ResponseValidationRedactionDescriptionWarningTest.java
@@ -318,7 +318,7 @@ public class ResponseValidationRedactionDescriptionWarningTest extends ProfileJs
 
         ArgumentCaptor<RDAPValidationResult> captor =
                 ArgumentCaptor.forClass(RDAPValidationResult.class);
-        verify(results, times(11)).add(captor.capture());
+        verify(results, times(12)).add(captor.capture());
 
         List<Integer> codes = captor.getAllValues().stream()
                 .map(RDAPValidationResult::getCode)
